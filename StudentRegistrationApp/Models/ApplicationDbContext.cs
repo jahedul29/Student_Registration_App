90cdcb2307f01e28d35a9deaa04c6342e39d18eb
jahedul29 <jahedulh1@gmail.com>
2020-01-13 00:29:41 +0600

    Setting up connection with database & Creating Student model

---
+++ StudentRegistrationApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentRegistrationApp.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentRegistrationApp/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistrationApp.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(6,18)]
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Class { get; set; }
+    }
+}
