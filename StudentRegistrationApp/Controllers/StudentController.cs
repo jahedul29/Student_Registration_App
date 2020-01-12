bf8078a006eb5f866f623ff39dda01dacc80bcba
jahedul29 <jahedulh1@gmail.com>
2020-01-13 00:53:54 +0600

    Index View page part 1

---
+++ StudentRegistrationApp/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentRegistrationApp.Models;
+
+namespace StudentRegistrationApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Students.ToList());
+        }
+    }
+}
