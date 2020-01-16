9c2d35b5acf1319c5d41f369c3b473a1b9bc64dd
jahedul29 <jahedulh1@gmail.com>
2020-01-16 08:44:13 +0600

    Final Upload

--- StudentRegistrationApp/Controllers/StudentController.cs
+++ StudentRegistrationApp/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,8 @@ namespace StudentRegistrationApp.Controllers
         {
             _db = db;
         }
+
+
         public IActionResult Index()
         {
             return View(_db.Students.ToList());
@@ -39,6 +42,87 @@ namespace StudentRegistrationApp.Controllers
 
             return View(student);
 
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _db.Students.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _db.Students.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var student = await _db.Students.SingleOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _db.Students.Remove(student);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET::Student/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                NotFound();
+            }
+
+            var student = await _db.Students.SingleOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                NotFound();
+            }
+
+            return View(student);
+        }
+
+        //POST::Student/Edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                var StudentDb = await _db.Students.FirstOrDefaultAsync(m => m.Id == student.Id);
+                StudentDb.Name = student.Name;
+                StudentDb.Age = student.Age;
+                StudentDb.Class = student.Class;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(student);
         }
     }
 }
